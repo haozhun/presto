3158524b21f1b6a2154d011c4a0f2c94a9965156
Haozhun Jin <haozhun.jin@gmail.com>
2016-09-23 17:49:24 -0700

    Introduce TypeSignatureProvider to SignatureBinder

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/ExpressionAnalyzer.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/ExpressionAnalyzer.java
@@ -728,14 +728,14 @@ public class ExpressionAnalyzer
                 }
             }
 
-            ImmutableList.Builder<TypeSignature> argumentTypes = ImmutableList.builder();
+            ImmutableList.Builder<TypeSignatureProvider> argumentTypesBuilder = ImmutableList.builder();
             for (Expression expression : node.getArguments()) {
-                argumentTypes.add(process(expression, context).getTypeSignature());
+                argumentTypesBuilder.add(new TypeSignatureProvider(process(expression, context).getTypeSignature()));
             }
 
             Signature function;
             try {
-                function = functionRegistry.resolveFunction(node.getName(), argumentTypes.build());
+                function = functionRegistry.resolveFunction(node.getName(), argumentTypesBuilder.build());
             }
             catch (PrestoException e) {
                 if (e.getErrorCode().getCode() == StandardErrorCode.FUNCTION_NOT_FOUND.toErrorCode().getCode()) {
